9f2f31abd2a00758eaabf1b94f6cfe03fd255779
gkstmdgus <tmdgus980318@gmail.com>
2025-01-14 10:22:22 +0900

    gruvbox 테마 삭제

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,9 +3,9 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require("lualine").setup({
-            options = {
-                theme = "gruvbox"
-            }
+            -- options = {
+            --     theme = "gruvbox"
+            -- }
         })
     end
 }
