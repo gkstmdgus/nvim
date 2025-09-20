5fd554e11a8a62c35ed23a7963d0637bae3455da
gkstmdgus <tmdgus980318@gmail.com>
2025-09-20 20:32:45 +0900

    neo-tree > space로 파일 열리도록 수정

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,7 +6,19 @@ return {
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    config = function()
+        local neo_tree = require("neo-tree")
+
+        neo_tree.setup({
+            window = {
+                mappings = {
+                    -- Space 키로 파일 열기
+                    ["<space>"] = "open",
+                },
+            },
+        })
+    end
 }
 
 --[[
