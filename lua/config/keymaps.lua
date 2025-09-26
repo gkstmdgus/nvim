118f64fcbfdb0f687006f7634959d6852119ec39
gkstmdgus <tmdgus980318@gmail.com>
2025-09-27 00:03:18 +0900

    buffer size 조절 단축키 추가

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,3 +21,9 @@ mapKey("<", "<gv", "v")
 mapKey("<leader>]", ":bn<cr>", "n", { desc = "Buffer [N]ext " })
 mapKey("<leader>[", ":bp<cr>", "n", { desc = "Buffer [P]revious " })
 mapKey("<leader>q", ":Bdelete<cr>", "n", { desc = "Buffer [Q]uit " })
+
+-- buffer size
+mapKey("<C-w>h", "<C-w><", "n", { noremap = true, silent = true, desc = "Narrow window" })
+mapKey("<C-w>l", "<C-w>>", "n", { noremap = true, silent = true, desc = "Widen window" })
+mapKey("<C-w>j", "<C-w>-", "n", { noremap = true, silent = true, desc = "Decrease the height of a window" })
+mapKey("<C-w>k", "<C-w>+", "n", { noremap = true, silent = true, desc = "Increase the height of a window" })
